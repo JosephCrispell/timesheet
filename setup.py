fed2e0f6a89fccad106f69e3164a126be482479a
Joseph Crispell <crispelljoseph@gmail.com>
2023-03-11 20:05:43 +0000

    Creating setup script

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="my_pip_package",
+    version="dev",
+    url="https://github.com/JosephCrispell/timesheet",
+    author="JosephCrispell",
+    author_email="test@mail.com",
+    py_modules=["timesheet"],
+)
