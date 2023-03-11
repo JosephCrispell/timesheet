bc60da11d382aff449781fc6a604a797dadb1b41
Joseph Crispell <crispelljoseph@gmail.com>
2023-03-11 20:10:34 +0000

    Added link to version

--- setup.py
+++ setup.py
@@ -1,8 +1,10 @@
 from setuptools import setup
 
+from timesheet import __version__
+
 setup(
     name="my_pip_package",
-    version="dev",
+    version=__version__,
     url="https://github.com/JosephCrispell/timesheet",
     author="JosephCrispell",
     author_email="test@mail.com",
