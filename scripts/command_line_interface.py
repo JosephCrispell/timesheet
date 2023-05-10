79d2dab86b352953b1b70a3ab4daa819c7a7de16
Joseph Crispell <crispelljoseph@gmail.com>
2023-05-10 19:52:54 +0100

    Improved argument information in parser

--- scripts/command_line_interface.py
+++ scripts/command_line_interface.py
@@ -1,5 +1,6 @@
 # Load packages
 import argparse  # parsing command line arguments
+from datetime import datetime  # working with dates and times
 
 # Local imports
 from timesheet import timesheet
@@ -22,16 +23,20 @@ def build_command_line_interface() -> argparse.ArgumentParser:
     welcome_message = "Welcome to timesheet, a tool to help you log the hours you work. You are using the command line interface for timesheet."
 
     # Initialize parser
-    parser = argparse.ArgumentParser(description=welcome_message)
+    parser = argparse.ArgumentParser(
+        description=welcome_message,
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,  # Shows default values for parameters
+    )
 
     # Add arguments
     parser.add_argument(
         "-f",
         "--file",
         nargs="?",  # Accept 0 or 1 arguments
-        const="outputs/timesheet.csv",  # Default value
+        default="outputs/timesheet.csv",  # Default value
+        metavar="timesheet_file_path",
         type=str,
-        help="Provide file for timesheet. Defaults to outputs/timesheet.csv",
+        help="Provide file for timesheet.",
     )
     parser.add_argument(
         "-r",
@@ -43,7 +48,8 @@ def build_command_line_interface() -> argparse.ArgumentParser:
         "-s",
         "--start",
         nargs="?",
-        const=None,
+        default=datetime.now().strftime("%H:%M"),
+        metavar="hh:mm",
         type=str,
         help="Add start time (hh:mm) to timesheet file provided with file (-f/--file) argument. Defaults to current time.",
     )
@@ -51,7 +57,8 @@ def build_command_line_interface() -> argparse.ArgumentParser:
         "-e",
         "--end",
         nargs="?",
-        const=None,
+        default=datetime.now().strftime("%H:%M"),
+        metavar="hh:mm",
         type=str,
         help="Add end time (hh:mm) to timesheet file provided with file (-f/--file) argument. Defaults to current time.",
     )
@@ -67,8 +74,8 @@ def main():
     # Get arguments
     args = parser.parse_args()
 
-    if args.Output:
-        print("Displaying Output as: % s" % args.Output)
+    # Check for timesheet file
+    print(args.file)
 
 
 if __name__ == "__main__":
