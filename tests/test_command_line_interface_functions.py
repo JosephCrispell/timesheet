f46f8516ddc5df79d3b8031a89f0893de953c57c
Joseph Crispell <crispelljoseph@gmail.com>
2023-05-10 20:39:21 +0100

    Added placeholder for testing cli functions

---
+++ tests/test_command_line_interface_functions.py
@@ -0,0 +1,15 @@
+# Load packages
+import unittest  # running tests
+
+# Local imports
+from timesheet import command_line_interface_functions  # cli functions
+
+
+# class TestCommandLineInterfaceFunctions(unittest.TestCase):
+
+# TODO add test for build_command_line_interface
+# TODO add test for parse_command_line_arguments
+
+
+if __name__ == "__main__":
+    unittest.main()
